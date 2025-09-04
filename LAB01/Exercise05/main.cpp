5cbc248249c96efb58e76073a2fe888488343eaf
IssamLRB <laribiissam8@gmail.com>
2025-09-04 19:41:18 +0100

    Update main.cpp

--- LAB01/Exercise05/main.cpp
+++ LAB01/Exercise05/main.cpp
@@ -32,7 +32,7 @@ public:
 
         // Bubble sort the numbers in descending order
         for (int i = 0; i < nums.size() - 1; i++) {
-            for (int j = 0; j < nums.size() - 1; j++) {
+            for (int j = 0; j < nums.size() - i - 1; j++) {
                 if (nums[j] < nums[j + 1]) swap(nums[j], nums[j + 1]);
             }
         }
