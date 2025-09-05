8169762d9026e03c00e93b30be9c05a970f1c80b
IssamLRB <laribiissam8@gmail.com>
2025-09-05 19:27:25 +0100

    Update main.cpp

--- LAB01/Exercise03/main.cpp
+++ LAB01/Exercise03/main.cpp
@@ -1,1 +1,24 @@
+#include "collection.hpp"
+using namespace std;
 
+int main() {
+    Collection<int> c;
+
+    c.insert(10);
+    c.insert(20);
+    c.insert(30);
+
+    cout << "The collection elements are: ";
+    c.display();
+
+    cout << "Does the collection contains 10?" << (c.contains(10) ? " Yes" : " No") << endl;
+
+    c.remove(10);
+    cout << "Element" << 10 << "is removed" << endl;
+    cout << "Does the collection contains 10?" << (c.contains(10) ? " Yes" : " No") << endl;
+
+    c.makeEmpty();
+    cout << "Is the collection empty? " << (c.isEmpty() ? "Yes" : "No") << endl;
+
+    return 0;
+}
