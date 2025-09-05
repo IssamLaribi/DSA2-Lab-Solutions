ad2fa824a1cbb37c33fc18631afd5fd91c996d6e
IssamLRB <laribiissam8@gmail.com>
2025-09-05 20:07:35 +0100

    Update ordered_collection.hpp

--- LAB01/Exercise04/ordered_collection.hpp
+++ LAB01/Exercise04/ordered_collection.hpp
@@ -1,1 +1,29 @@
+#ifndef E4AB4CA7_94EA_4F95_8972_39D6C09E9C60
+#define E4AB4CA7_94EA_4F95_8972_39D6C09E9C60
+
+#include <vector>
+using namespace std;
+
+template <typename Comparable>
+class OrderedCollection {
+private: 
+    vector<Comparable> data;
+public: 
+    OrderedCollection();
+
+    bool isEmpty() const;
+    void makeEmpty();
+    void insert(const Comparable &x);
+    void remove(const Comparable &x);
+
+    Comparable& findMin(const Comparable &x);
+    Comparable& findMax(const Comparable &x);
+
+    bool contains(const Comparable& x) const;
+    void display() const;
+};
+
+#include "ordered_collection.tpp"
+
+#endif /* E4AB4CA7_94EA_4F95_8972_39D6C09E9C60 */
 
