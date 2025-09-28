b3ddb2a7787376bf3cd481eeb374667e1d836210
IssamLRB <laribiissam8@gmail.com>
2025-09-28 02:44:53 +0100

    Update main.cpp

--- LAB01/Exercise02/main.cpp
+++ LAB01/Exercise02/main.cpp
@@ -3,7 +3,19 @@
 #include <vector>
 using namespace std;
 
-    
+void permutateWord(string word, int l, int r) {
+    if (l == r) {
+        cout << word << endl;
+        return;
+    }
+
+    for(int i = l; i <= r; i++) {
+        swap(word[l], word[i]);
+        permutateWord(word, l + 1, r);
+        swap(word[l], word[i]);
+    }
+}
+
 
 int main() {
     string word = "ABC";
